fdfefc412bd1226c89b9742d7990c91d7eef2610
Tomasz Bartczak <kretes@gazeta.pl>
2014-03-14 13:57:43 +0100

    moved worklog parsing to service

--- src/main/webapp/app/RegistrationCtrl.js
+++ src/main/webapp/app/RegistrationCtrl.js
@@ -1,74 +1,15 @@
 angular.module('openTrApp').controller('RegistrationCtrl',
-		function($scope, $http, timeProvider) {
+		function($scope, $http, timeProvider, worklogEntryParser) {
 
-			var projectPattern = /#([a-zA-Z0-9_]*)?/;
-			var workloadPattern = /(\d+(d|h|m)( )?)+/;
-			var dayPattern = /@([0-9\/]*)/;
 
-            var getWorkloadFromExpression = function(expression) {
-                if (workloadPattern.test(expression)) {
-                    return workloadPattern.exec(expression)[0].trim()
-                } else {
-                    return "1d";
-                }
-            }
-
-			var parseExpression = function(expression){
-
-                return {
-                    workload: getWorkloadFromExpression(expression),
-                    projectName: projectPattern.exec(expression)[1],
-                    day: getDayFromExpression(expression)
-                };
-			};
-
-            function matchesDayRegex(expression) {
-                return dayPattern.test(expression);
-            }
-
-            function dayRegexHasValidDate(expression) {
-                return moment(dayPattern.exec(expression)[1]).isValid();
-            }
-
-            var getDayFromExpression = function(expression) {
-                if(matchesDayRegex(expression) && dayRegexHasValidDate(expression)) {
-                    return dayPattern.exec(expression)[1]
-                } else if (forYesterday(expression)) {
-                    return moment(timeProvider.getCurrentDate()).subtract('days', 1).format("YYYY/MM/DD")
-                } else {
-                    return moment(timeProvider.getCurrentDate()).format("YYYY/MM/DD");
-                }
-            }
-
-            function hasDayExpression(expression) {
-                return /@/.test(expression);
-            }
-
-            function hasValidDateExpression(expression) {
-                return (hasDayExpression(expression) && dayRegexHasValidDate(expression));
-            }
-
-            function forYesterday(expression) {
-                return /@yesterday/.test(expression);
-            }
-
-            var dayValid = function(expression) {
-                return hasValidDateExpression(expression) || forYesterday(expression) || !hasDayExpression(expression)
-            }
-
-            var isValid = function(expression){
-				return projectPattern.test(expression) &&
-                    dayValid(expression)
-			};
-	
 			//	2h on #ProjectManhattan @2014/01/03
 			$scope.logWork = function(){
 				
-				if(!isValid($scope.workLogExpression)){
+				if(!worklogEntryParser.isValid($scope.workLogExpression)){
 					return;
 				}
 				
-				var data = parseExpression($scope.workLogExpression);
+				var data = worklogEntryParser.parse($scope.workLogExpression);
 				$http
 					.post('http://localhost:8080/endpoints/v1/employee/1/work-log/entries', data)
 					.success(function(response, status){
@@ -90,7 +31,7 @@ angular.module('openTrApp').controller('RegistrationCtrl',
 					return;
 				}
 				
-				if(isValid($scope.workLogExpression)){
+				if(worklogEntryParser.isValid($scope.workLogExpression)){
 					$scope.status = 'success';
 				} else{
 					$scope.status = 'error';
---
+++ src/main/webapp/app/WorkLogEntryParser.js
@@ -0,0 +1,61 @@
+angular.module('openTrApp').factory('worklogEntryParser', function(timeProvider){
+
+    var projectPattern = /#([a-zA-Z0-9_]*)?/;
+    var workloadPattern = /(\d+(d|h|m)( )?)+/;
+    var dayPattern = /@([0-9\/]*)/;
+
+    var getWorkloadFromExpression = function(expression) {
+        if (workloadPattern.test(expression)) {
+            return workloadPattern.exec(expression)[0].trim()
+        } else {
+            return "1d";
+        }
+    }
+
+    function matchesDayRegex(expression) {
+        return dayPattern.test(expression);
+    }
+
+    function dayRegexHasValidDate(expression) {
+        return moment(dayPattern.exec(expression)[1]).isValid();
+    }
+
+    var getDayFromExpression = function(expression) {
+        if(matchesDayRegex(expression) && dayRegexHasValidDate(expression)) {
+            return dayPattern.exec(expression)[1]
+        } else if (forYesterday(expression)) {
+            return moment(timeProvider.getCurrentDate()).subtract('days', 1).format("YYYY/MM/DD")
+        } else {
+            return moment(timeProvider.getCurrentDate()).format("YYYY/MM/DD");
+        }
+    }
+
+    function hasDayExpression(expression) {
+        return /@/.test(expression);
+    }
+
+    function hasValidDateExpression(expression) {
+        return (hasDayExpression(expression) && dayRegexHasValidDate(expression));
+    }
+
+    function forYesterday(expression) {
+        return /@yesterday/.test(expression);
+    }
+
+    var dayValid = function(expression) {
+        return hasValidDateExpression(expression) || forYesterday(expression) || !hasDayExpression(expression)
+    }
+
+    return{
+        isValid: function(expression) {
+            return projectPattern.test(expression) && dayValid(expression)
+        },
+        parse: function(expression) {
+            return {
+                workload: getWorkloadFromExpression(expression),
+                projectName: projectPattern.exec(expression)[1],
+                day: getDayFromExpression(expression)
+            };
+        }
+    }
+});
