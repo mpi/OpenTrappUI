99501856b7883dbcfe2ce24a7393cecfc8b2b4cc
mpi <mpi.michal.piotrkowski+gh@gmail.com>
2014-03-24 20:17:48 +0100

    closes #25, #21

--- src/main/webapp/app/AuthInterceptor.js
+++ src/main/webapp/app/AuthInterceptor.js
@@ -1,21 +1,9 @@
-angular.module('openTrApp').factory('authInterceptor', function($cookies){
+angular.module('openTrApp').factory('authInterceptor', function($cookies, $location){
 
-	var getParameterByName = function(name) {
-		   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-		   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-		       results = regex.exec(window.location.search);
-		   return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-	};
-	
 	return {
 		
 		request: function(config){
 
-			var authToken = getParameterByName("authToken");
-			if(authToken){
-				$cookies.authToken = authToken;
-			}
-			
 			if($cookies.authToken && config.url.indexOf('/endpoints/') != -1){
 				config.url = config.url + ';jsessionid=' + $cookies.authToken;
 			}
--- src/main/webapp/app/SignInCtrl.js
+++ src/main/webapp/app/SignInCtrl.js
@@ -1,5 +1,5 @@
 angular.module('openTrApp').controller('SignInCtrl',
-    function ($scope, $http, $location, currentEmployee) {
+    function ($scope, $rootScope, $http, $location, currentEmployee) {
 		$scope.init = function(){
             $http.get('http://localhost:8080/endpoints/v1/authentication/status').success(function (data) {
                 $scope.displayName = data.displayName;
@@ -11,5 +11,9 @@ angular.module('openTrApp').controller('SignInCtrl',
                 currentEmployee.signedInAs(data.username);
             });
 		};
+		$rootScope.$on('AuthTokenReceived', function(){
+			console.log('AuthTokenReceived');
+			$scope.init();
+		});
 	}
 );
