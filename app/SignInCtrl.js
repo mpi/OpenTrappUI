186cdd2fa317a51c1d9b09a36042c83d7d44b243
mpi <mpi.michal.piotrkowski+gh@gmail.com>
2014-03-25 22:25:19 +0100

    Publish of github pages from Gradle

--- app/SignInCtrl.js
+++ app/SignInCtrl.js
@@ -4,6 +4,7 @@ angular.module('openTrApp').controller('SignInCtrl',
             $http.get('http://localhost:8080/endpoints/v1/authentication/status').success(function (data) {
                 $scope.displayName = data.displayName;
                 $scope.authenticated = data.authenticated;
+                $scope.unauthenticated = !data.authenticated;
                 $scope.username = data.username;
                 $scope.loginUrl = data.loginUrl + "?redirect_to=" + $location.absUrl();
                 $scope.logoutUrl = data.logoutUrl + "?redirect_to=" + $location.absUrl();
